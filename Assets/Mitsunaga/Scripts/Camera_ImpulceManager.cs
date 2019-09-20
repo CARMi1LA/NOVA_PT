387f16f744c477fec72e913672e3ac4d3eebe709
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-09-20 14:46:49 +0900

    プレイヤーのモデル、弾のマテリアル作成してプレファブ化

---
+++ Assets/Mitsunaga/Scripts/Camera_ImpulceManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using Cinemachine;
+
+public class Camera_ImpulceManager : MonoBehaviour
+{
+    // Cinemachineを利用して、画面の揺れを実装する
+    CinemachineImpulseSource CISource;
+
+    [SerializeField,Header("衝撃の大きさ")]
+    Vector3 impulsePower;
+
+    // 揺れを発生させるストリーム
+    public BoolReactiveProperty impulseRP = new BoolReactiveProperty(false);
+
+    void Awake()
+    {
+        CISource = this.GetComponent<CinemachineImpulseSource>();
+    }
+    void Start()
+    {
+        // 
+        impulseRP
+            .Where(x => x)
+            .Subscribe(_ =>
+            {
+                CISource.GenerateImpulse(impulsePower);
+                impulseRP.Value = false;
+            })
+            .AddTo(this.gameObject);
+
+        this.UpdateAsObservable()
+            .Where(x => Input.GetKeyDown(KeyCode.I))
+            .Subscribe(_ =>
+            {
+                impulseRP.Value = true;
+            })
+            .AddTo(this.gameObject);
+    }
+}
