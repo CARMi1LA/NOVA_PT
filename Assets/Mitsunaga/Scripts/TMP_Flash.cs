89bb6c6d65c3f6ad96e5610c2cb50eb9e35c669d
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-10-02 15:07:38 +0900

    タイトルおよびステージセレクトシーンの実装

---
+++ Assets/Mitsunaga/Scripts/TMP_Flash.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using TMPro;
+
+public class TMP_Flash : MonoBehaviour
+{
+    // TextMeshProUGUI を点滅させる
+
+    TextMeshProUGUI tmpFlash;
+
+    void Start()
+    {
+        tmpFlash = GetComponent<TextMeshProUGUI>();
+
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                float alpha = Mathf.PingPong(Time.time, 1.0f);
+
+                tmpFlash.color = new Color(tmpFlash.color.r, tmpFlash.color.g, tmpFlash.color.b, alpha);
+            })
+            .AddTo(this.gameObject);
+    }
+}
