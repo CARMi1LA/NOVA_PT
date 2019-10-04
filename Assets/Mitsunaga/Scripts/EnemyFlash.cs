f4d72590df226db40b3ffc3aab3a76f1669a3fa9
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-10-04 11:24:00 +0900

    キャラクターのダメージ時点滅機能の追加

---
+++ Assets/Mitsunaga/Scripts/EnemyFlash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlash : MonoBehaviour
+{
+    [SerializeField]
+    Renderer enemyRenderer;
+
+    [SerializeField]
+    bool isDamage;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (isDamage)
+        {
+            enemyRenderer.material.SetInt("_IsDamage", 1);
+        }
+        else
+        {
+            enemyRenderer.material.SetInt("_IsDamage", 0);
+
+        }
+    }
+}
