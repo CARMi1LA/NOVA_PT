6b48e4e275123e759e079ddf28ce91ff19d64035
nkcla <nkc.sota.mitsunaga@gmail.com>
2019-10-06 00:38:53 +0900

    ボスのシェーダー作成

---
+++ Assets/Mitsunaga/Scripts/AutoRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class AutoRotation : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 rotSpeed;
+
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                this.transform.localEulerAngles += rotSpeed * Time.deltaTime;
+            })
+            .AddTo(this.gameObject);
+    }
+}
