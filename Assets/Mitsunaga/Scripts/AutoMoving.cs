132e4850b4cc92b45d31e00b617f368456da4352
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-10-08 11:11:14 +0900

    Amina2Dの追加、2Dキャラクター作成

---
+++ Assets/Mitsunaga/Scripts/AutoMoving.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class AutoMoving : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 moveSpeed;
+    [SerializeField]
+    bool isPingPong;
+    [SerializeField]
+    float timeScale;
+
+    void Start()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 movePos;
+
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                if (isPingPong)
+                {
+                    movePos = startPos + moveSpeed * Mathf.Sin(Time.time * timeScale);
+                }
+                else
+                {
+                    movePos = startPos + moveSpeed * Time.deltaTime;
+                }
+
+                transform.position = movePos;
+            })
+            .AddTo(this.gameObject);
+    }
+}
