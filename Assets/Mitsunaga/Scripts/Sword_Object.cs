02047e586dab2478dbfd2ab91b53dcd0abd16c09
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-10-19 02:14:15 +0900

    回転する剣の作成中

---
+++ Assets/Mitsunaga/Scripts/Sword_Object.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+
+public class Sword_Object : MonoBehaviour
+{
+    [SerializeField]
+    Sword_Rotation sRot;
+    [SerializeField]
+    float generateTime;
+
+    ReactiveProperty<float> alphaRP = new ReactiveProperty<float>();
+
+    void Start()
+    {
+        sRot.SwordSubject
+            .Subscribe(value =>
+            {
+                StartCoroutine(ChangeAlphaCoroutine(value, generateTime));
+            })
+            .AddTo(this.gameObject);
+
+        alphaRP
+            .Subscribe(value =>
+            {
+                Debug.Log(alphaRP.Value.ToString());
+            })
+            .AddTo(this.gameObject);
+    }
+
+    IEnumerator ChangeAlphaCoroutine(float alpha, float time)
+    {
+        float t = 0.0f;
+        float startAlpha = alphaRP.Value;
+
+        while(t < time)
+        {
+            t += Time.deltaTime;
+
+            alphaRP.Value = Mathf.Lerp(startAlpha, alpha, t / time);
+
+            yield return null;
+        }
+
+        alphaRP.Value = alpha;
+    }
+}
---
+++ Assets/Mitsunaga/Scripts/Sword_Rotation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Sword_Rotation : MonoBehaviour
+{
+    // 剣による近接攻撃
+    /*
+    剣の生成
+        ShaderのAlpha値の変更
+        当たり判定を追加
+    剣の回転
+        Sinカーブによる回転の実装
+    剣の消滅
+        当たり判定の削除
+        ShaderのAlpha値を変更
+    */
+
+    [SerializeField]    // 回転速度
+    float rotTime;
+
+    public Subject<float> SwordSubject = new Subject<float>();
+
+    // 剣の生成
+    public ReactiveProperty<bool> SwordGenerateRP = new ReactiveProperty<bool>();
+    [SerializeField] float waitSwordGenerate;   // 剣の生成の待ち時間
+
+    bool isStandby = false;  // 既に実行しているか
+    Vector3 startRotation;  // 回転量の初期値
+
+    void Start()
+    {
+        // 初期設定
+        startRotation = this.transform.localEulerAngles;
+
+        // 剣の生成→回転→消滅　イベント
+        SwordSubject
+            .Where(x => isStandby)                          // 待機中のみ実行
+            .Do(_ =>                                        // 剣の生成
+            {
+                isStandby = false;
+                SwordGenerateRP.Value = true;
+            })
+            .Delay(TimeSpan.FromSeconds(waitSwordGenerate)) // 生成中の待機時間
+            .Subscribe(value =>                             // 剣の回転
+            {
+                // 剣の回転コルーチンの再生
+                Observable.FromCoroutine(_ => RotationCoroutine(value))
+                .Subscribe(
+                    _  => Debug.Log("Rotatin OnNext"),
+                    () => 
+                    {
+                        SwordGenerateRP.Value = false;      // 剣の消滅
+                        isStandby = true;
+                    })
+                    .AddTo(this.gameObject);
+            })
+            .AddTo(this.gameObject);
+
+        // デバッグ用のイベント発行
+        this.OnMouseDownAsObservable()
+            .Subscribe(_ =>
+            {
+                SwordSubject.OnNext(rotTime);
+            })
+            .AddTo(this.gameObject);
+    }
+
+    // 剣の回転コルーチン
+    IEnumerator RotationCoroutine(float time)
+    {
+        float t = 0.0f;
+        float rot = 0.0f;
+
+        while (t < time)
+        {
+            rot = Mathf.Lerp(this.transform.eulerAngles.y, 360.0f, 0.05f);
+
+            transform.localEulerAngles = new Vector3(0, rot, 0);
+
+            t += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.localEulerAngles = startRotation;
+    }
+}
