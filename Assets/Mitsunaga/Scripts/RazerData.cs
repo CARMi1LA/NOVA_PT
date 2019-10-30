99c7983ce2b1dbdb23bbf92535e62500234787b0
DESKTOP-28F38VP\user <nkc.sota.mitsunaga@gmail.com>
2019-10-31 00:42:50 +0900

    ボス警告の見た目作成、レーザーの処理作成中

---
+++ Assets/Mitsunaga/Scripts/RazerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazerData : MonoBehaviour
+{
+    // レーザー生成素材
+
+    public enum RazerParent
+    {
+        Null = 0,
+        Player = 1,
+        Enemy = 2,
+        Boss = 3
+    }
+    public RazerParent rParent; // 生成元
+    public float rDelay;        // 発射時の待機時間
+    public Vector3 rPosition;   // 発射の始点
+    public Vector3 rRotation;   // 発射の方向
+
+    // コンストラクタ
+    public RazerData(RazerParent rp,float delay,Vector3 position,Vector3 euler)
+    {
+        rParent = rp;
+        rDelay = delay;
+        rPosition = position;
+        rRotation = euler;
+    }
+}
---
+++ Assets/Mitsunaga/Scripts/RazerManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class RazerManager : MonoBehaviour
+{
+    [SerializeField]
+    float waitTime = 1.0f;
+    [SerializeField]
+    Transform pT;
+
+    float maxRayDistance = 50.0f;
+
+    public Subject<RazerData> RazerSubject = new Subject<RazerData>();
+
+
+    void Start()
+    {
+        // デバッグ用
+        this.UpdateAsObservable()
+            .Where(x => Input.GetKeyDown(KeyCode.R))
+            .Subscribe(_ =>
+            {
+                RazerData rd = new RazerData(RazerData.RazerParent.Player, 0.5f, pT.localPosition, pT.localEulerAngles);
+
+                RazerSubject.OnNext(rd);
+            })
+            .AddTo(this.gameObject);
+
+        RazerSubject
+            .Subscribe(value =>
+            {
+                // ディレイの長さを可変にしたいが長さをRazerDataに格納すると.Delay()が使えないためTimerを用いる
+                Observable
+                .Timer(System.TimeSpan.FromSeconds(value.rDelay))
+                .Subscribe(_ =>
+                {
+                    // RaycastAll(Ray(始点,方向), float(距離))
+                    // ・主にForeach内で使い、それぞれ対象をRaycastHit型に入れて処理を行う
+                    Ray r = new Ray(value.rPosition, value.rRotation);
+                    foreach(RaycastHit hit in Physics.RaycastAll(r, maxRayDistance))
+                    {
+                        // タグがターゲットか否かを判断する
+                        if(hit.transform.tag == ((value.rParent == RazerData.RazerParent.Player)? "Player" : "Enemy"))
+                        {
+                            // RaycastHit.collider.gameObject で触れたオブジェクトの情報を取り出せる
+                            hit.collider.gameObject.GetComponent<IDamage>().HitDamage();
+                        }
+                    }
+                })
+                .AddTo(this.gameObject);
+            })
+            .AddTo(this.gameObject);
+    }
+}
