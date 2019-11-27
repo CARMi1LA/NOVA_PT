5d7e9fad5da647a1f77a752441e6ccb4d84e09db
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2019-11-27 14:34:08 +0900

    データ追加

--- Assets/Kakihana/Scripts/DataList/ShopData.cs
+++ Assets/Kakihana/Scripts/DataList/ShopData.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ShopData : MonoBehaviour
 {
+    public int[,] level_Player;     // プレイヤーの各パラメータレベル（パラメーター名,レベル）
+    public int[,,] level_Tower;     // タワーの各パラメータレベル（タワー番号,パラメーター名,レベル）
+    public int[] level_Ult;        // スキル＆必殺技の各パラメータ
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
