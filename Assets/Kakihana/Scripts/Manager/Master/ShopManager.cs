cb671a78d7231c9d3cea817e43e73a46c7ece6a8
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2019-12-03 15:05:20 +0900

    ショップUIの枠組み完成

--- Assets/Kakihana/Scripts/Manager/Master/ShopManager.cs
+++ Assets/Kakihana/Scripts/Manager/Master/ShopManager.cs
@@ -21,7 +21,7 @@ public class ShopManager : SPMSinleton<ShopManager>
     public Subject<ShopData.TowerYellow_ParamList> addLevel_TowerYellow = new Subject<ShopData.TowerYellow_ParamList>();
     public Subject<ShopData.TowerGreen_ParamList> addLevel_TowerGreen = new Subject<ShopData.TowerGreen_ParamList>();
     public Subject<ShopData.Skill_ParamList> addLevel_Skill = new Subject<ShopData.Skill_ParamList>();
-    public Subject<ShopData.Player_ParamList> addLevel_Ult = new Subject<ShopData.Player_ParamList>();
+    public Subject<ShopData.Ult_ParamList> addLevel_Ult = new Subject<ShopData.Ult_ParamList>();
 
     protected override void Awake()
     {
@@ -33,44 +33,88 @@ public class ShopManager : SPMSinleton<ShopManager>
     // Start is called before the first frame update
     void Start()
     {
-        addLevel_Player.Subscribe(_ => 
-        {
+        addLevel_Player
+            .Where(val => mater >= shopData.shopData_Player[(int)val].purchaseMater)
+            .Subscribe(val => 
+            {
+                switch (val)
+                {
+                    case ShopData.Player_ParamList.Param_HP:
+                        shopData.levelData_Player.level_HP.Value++;
+                        break;
+                    case ShopData.Player_ParamList.Param_Speed:
+                        shopData.levelData_Player.level_Speed.Value++;
+                        break;
+                    case ShopData.Player_ParamList.Param_Interval:
+                        shopData.levelData_Player.level_Interval.Value++;
+                        break;
+                }
+            }).AddTo(this.gameObject);
 
-        }).AddTo(this.gameObject);
+        addLevel_TowerRed
+            .Subscribe(val => { }).AddTo(this.gameObject);
+
+        addLevel_Skill
+            .Where(val => mater >= shopData.shopData_Skill[(int)val].purchaseMater)
+            .Subscribe(val =>
+            {
+                if ((int)val == shopData.levelData_Skill.level_Skill.Value)
+                {
+
+                }
+                else
+                {
+                    shopData.levelData_Skill.level_Skill.Value = (int)val;
+                }
+            }).AddTo(this.gameObject);
+
+        addLevel_Ult
+            .Where(val => mater >= shopData.shopData_Ult[(int)val].purchaseMater)
+            .Subscribe(val =>
+            {
+                if ((int)val == shopData.levelData_Ult.level_Ult.Value)
+                {
+
+                }
+                else
+                {
+                    shopData.levelData_Ult.level_Ult.Value = (int)val;
+                }
+            }).AddTo(this.gameObject);
     }
 
     public void OnClickPlayerParam(ShopData.Player_ParamList player_Param)
     {
-
+        addLevel_Player.OnNext(player_Param);
     }
 
     public void OnClickTowerParam(ShopData.TowerRed_ParamList towerRed_Param)
     {
-
+        addLevel_TowerRed.OnNext(towerRed_Param);
     }
 
     public void OnClickTowerParam(ShopData.TowerBlue_ParamList towerBlue_Param)
     {
-
+        addLevel_TowerBlue.OnNext(towerBlue_Param);
     }
 
     public void OnClickTowerParam(ShopData.TowerYellow_ParamList towerYellow_Param)
     {
-
+        addLevel_TowerYellow.OnNext(towerYellow_Param);
     }
 
     public void OnClickTowerParam(ShopData.TowerGreen_ParamList towerGreen_Param)
     {
-
+        addLevel_TowerGreen.OnNext(towerGreen_Param);
     }
 
-    public void OnClickSkillParam()
+    public void OnClickSkillParam(ShopData.Skill_ParamList skill_ParamList)
     {
-
+        addLevel_Skill.OnNext(skill_ParamList);
     }
 
-    public void OnClickUltParam()
+    public void OnClickUltParam(ShopData.Ult_ParamList ult_ParamList)
     {
-
+        addLevel_Ult.OnNext(ult_ParamList);
     }
 }
