b11ef4159d82d3903172e4eaa93401877f1086c1
nkcla <nkc.sota.mitsunaga@gmail.com>
2020-01-07 20:37:05 +0900

    スポナーつくってます(あと生成本体部分)

--- Assets/Mitsunaga/TowerDefence/Enemy/TDEnemyWaveList.cs
+++ Assets/Mitsunaga/TowerDefence/Enemy/TDEnemyWaveList.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TDEnemyWaveList : MonoBehaviour
+// １つのウェーブ
+[CreateAssetMenu(fileName ="TDEnemyWaveList", menuName = "TDScriptable/TDEnemyWaveList", order = 3)]
+public class TDEnemyWaveList : ScriptableObject
 {
-    public List<TDEnemyWave> enemyWaveList;
-
-    public TDEnemyWave GetEnemyWave(int waveCount)
-    {
-        return enemyWaveList[waveCount];
-    }
+    public float enemyWaveInterval = 5.0f;
+    public List<TDEnemyWave> enemyWave;
 }
 
+// １つの間隔
 [System.Serializable]
 public class TDEnemyWave
 {
-
+    public List<TDList.EnemySizeList> enemyWavePart;
 }
