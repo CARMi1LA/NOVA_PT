a3e852ce7b0c7c413fee1a9eb215562109614e50
nkcla <nkc.sota.mitsunaga@gmail.com>
2020-01-07 17:23:16 +0900

    ファイルだけ？

---
+++ Assets/Mitsunaga/TowerDefence/Enemy/TDEnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class TDEnemySpawner : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Mitsunaga/TowerDefence/Enemy/TDEnemyWaveList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDEnemyWaveList : MonoBehaviour
+{
+    public List<TDEnemyWave> enemyWaveList;
+
+    public TDEnemyWave GetEnemyWave(int waveCount)
+    {
+        return enemyWaveList[waveCount];
+    }
+}
+
+[System.Serializable]
+public class TDEnemyWave
+{
+
+}
